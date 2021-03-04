aa60fc9529620c9f443d9ed962fe182e47dc5486
rgc <rgc@arzg.my.domain>
2021-03-04 20:05:44 +0900

    redo some of 6e9ad

--- train_test.py
+++ train_test.py
@@ -94,6 +94,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=0.1)
     parser.add_argument('--batchsize', type=int, default=50)
     parser.add_argument('--epoch', type=int, default=500)
+    parser.add_argument('--penalty', type=str, default='L2')
     args = parser.parse_args()
 
     X, Y = get_data()
