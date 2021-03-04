95bf9a74ac8c9f99add62f27b3707ef9972360a3
rgc <rgc@arzg.my.domain>
2021-03-04 19:45:16 +0900

    chainer7.7 configuration has changed

--- svm.py
+++ svm.py
@@ -15,19 +15,19 @@ class SVM(chainer.FunctionSet):
         self.penalty = penalty
 
     def forward(self, x, t, train=True):
+        chainer.config.train = not train
         xp = cuda.get_array_module(*x)
 
-        x = Variable(x, volatile=not train)
-        t = Variable(t, volatile=not train)
+        x = Variable(x)
+        t = Variable(t)
         h = self.fc(x)
         loss = hinge(h, t, self.penalty)
 
         if self.penalty == 'l1':
-            loss += self.c * F.sum(Variable(abs(self.fc.W),
-                                            volatile=not train))
+            loss += self.c * F.sum(Variable(abs(self.fc.W)))
 
         elif self.penalty == 'l2':
-            n = Variable(self.fc.W.dot(self.fc.W.T), volatile=not train)
+            n = Variable(self.fc.W.dot(self.fc.W.T))
             loss += self.c * F.reshape(n, ())
 
         return loss
