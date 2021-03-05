2f0842c0719f00d666eb692b06a95d9f838a75a9
rgc <rgc@arzg.my.domain>
2021-03-06 07:49:30 +0900

    change penalty f

--- train_test.py
+++ train_test.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=0.1)
     parser.add_argument('--batchsize', type=int, default=50)
     parser.add_argument('--epoch', type=int, default=500)
-    parser.add_argument('--penalty', type=str, default='L2')
+    parser.add_argument('--penalty', type=str, default='L1')
     args = parser.parse_args()
 
     X, Y = get_data()
